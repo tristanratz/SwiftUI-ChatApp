44784b7a26394cde85be4f7529dcbd974f93de50
Tristan Ratz <triratz@aol.com>
2019-09-24 23:46:46 +0200

    Add Server

---
+++ Server/main.py
@@ -0,0 +1,73 @@
+from twisted.internet.protocol import Factory,Protocol
+from twisted.internet import reactor
+
+class Chat(Protocol):
+
+    def __init__(self, users):
+        self.users = users
+        self.name = None
+        self.state = "GETNAME"
+
+    def connectionMade(self):
+        print("Connection made")
+        self.transport.write('server:inf:name'.encode("utf8"))
+
+    def makeConnection(self, transport):
+        super().makeConnection(transport)
+
+    def connectionLost(self, reason):
+        if self.name in self.users:
+            print(self.name + ' left the chat')
+            del self.users[self.name]
+        else:
+            print('Somebody left the chat')
+
+    def dataReceived(self, line):
+        print("Data input")
+        if self.state == "GETNAME":
+            self.handle_get_name(line.decode())
+        else:
+            self.handle_chat(line.decode())
+
+    def handle_get_name(self, name):
+        if name in self.users:
+            print("Username already taken (%s)" % name)
+            self.transport.write("server:inf:name".encode("utf8"))
+            return
+        print("New user signed up (%s)" % name)
+        self.transport.write(("server:msg:Welcome, %s!" % (name)).encode("utf8"))
+        self.name = name
+        self.users[name] = self
+        self.state = "CHAT"
+
+        for name, protocol in self.users.items():
+            if name != self.name:
+                protocol.transport.write(("server:msg:%s joined." % self.name).encode("utf8"))
+
+    def handle_chat(self, message):
+        print("New message")
+        m = message.split(":")
+        if m[0] != self.name:
+            return
+        message = "%s:msg:%s" % (self.name, m[2])
+        for name, protocol in self.users.items():
+            if self != protocol:
+                protocol.transport.write(message.encode("utf8"))
+
+
+class ChatFactory(Factory):
+
+    def __init__(self):
+        self.users = {} # maps user names to Chat instances
+
+    def buildProtocol(self, addr):
+        return Chat(self.users)
+
+def main():
+    reactor.listenTCP(8000, ChatFactory())
+    reactor.run()
+
+
+# this only runs if the module was *not* imported
+if __name__ == '__main__':
+    main()
